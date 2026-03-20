43a9a15aa07e0b855415660a6fce92248ba6bf3e
Koreia01 <arthurcss58@gmail.com>
2026-03-20 01:28:54 -0300

    primeiro commit backend ovos

---
+++ routes/pedidos.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+
+let pedidos = [];
+
+router.post("/pedido", (req, res) => {
+  const { nome, produto, endereco } = req.body;
+
+  if (!nome || !produto) {
+    return res.status(400).json({ erro: "Dados incompletos" });
+  }
+
+  const novoPedido = {
+    id: pedidos.length + 1,
+    nome,
+    produto,
+    endereco
+  };
+
+  pedidos.push(novoPedido);
+
+  res.json({
+    mensagem: "Pedido criado!",
+    pedido: novoPedido
+  });
+});
+
+router.get("/pedidos", (req, res) => {
+  res.json(pedidos);
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const pedidosRoutes = require("./routes/pedidos");
+app.use("/api", pedidosRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Backend rodando 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
